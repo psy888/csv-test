2513bccef353b0736dad52440115a76aaaac2bc8
psy888 <tbilisi.zp.snab@gmail.com>
2020-09-20 16:17:42 +0300

    init

---
+++ src/test/java/com/psy/csv/CsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psy.csv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
