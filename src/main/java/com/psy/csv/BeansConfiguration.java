a83c2de315eb111cdfbbd5f428908d50f99cd07b
psy888 <tbilisi.zp.snab@gmail.com>
2020-09-27 23:40:44 +0300

    add mapper bean and fix cors origin issue

--- src/main/java/com/psy/csv/BeansConfiguration.java
+++ src/main/java/com/psy/csv/BeansConfiguration.java
@@ -4,8 +4,13 @@ import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 public class BeansConfiguration {
 
     @Bean
@@ -13,4 +18,19 @@ public class BeansConfiguration {
         return new DozerBeanMapper();
     }
 
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
