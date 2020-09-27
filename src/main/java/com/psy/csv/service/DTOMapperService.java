6ee18ad306c79d409b656bcae1f0fe4a3e72fee9
psy888 <tbilisi.zp.snab@gmail.com>
2020-09-27 23:50:08 +0300

    add java doc

--- src/main/java/com/psy/csv/service/DTOMapperService.java
+++ src/main/java/com/psy/csv/service/DTOMapperService.java
@@ -14,6 +14,13 @@ public class DTOMapperService {
 
     Mapper mapper;
 
+    /**
+     * Map DTO to Entity (with DOZER)
+     *
+     * @param list  - list of parsed objects
+     * @param clazz - Entity Class (define map strategy)
+     * @return List of Entities
+     */
     public List map(List<CsvBean> list, Class clazz) {
         //convert  parsed data to entity list
         return list.stream().map(e -> mapper.map(e, clazz)).collect(Collectors.toList());
