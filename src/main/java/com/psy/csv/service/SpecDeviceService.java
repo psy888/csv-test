e0b1cf3c63d4202322bd584897f749bbb620a241
psy888 <tbilisi.zp.snab@gmail.com>
2020-09-27 23:51:40 +0300

    add base spec device service

---
+++ src/main/java/com/psy/csv/service/SpecDeviceService.java
@@ -0,0 +1,50 @@
+package com.psy.csv.service;
+
+import com.psy.csv.dto.CsvBean;
+import com.psy.csv.entity.CSVFile;
+import com.psy.csv.entity.SpecDevice;
+import com.psy.csv.repository.SpecDeviceRepository;
+import com.psy.csv.util.PageAndSortUtil;
+import lombok.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Value
+public class SpecDeviceService implements DeviceService {
+
+    SpecDeviceRepository repository;
+    DTOMapperService dtoMapperService;
+
+    /**
+     * add new parsed and mapped devices to db
+     *
+     * @param dtoDevices - list of mapped dto devices
+     * @param file       - uploaded file dependency for each device
+     */
+    public void addNewDevices(List<CsvBean> dtoDevices, CSVFile file) {
+
+        List<SpecDevice> entityList = dtoMapperService.map(dtoDevices, SpecDevice.class); //map dto to entities
+
+        entityList.forEach(o -> o.setFile(file)); //set source file field
+
+        saveAll(entityList);// save all entities
+    }
+
+    public void saveAll(List<SpecDevice> list) {
+        repository.saveAll(list);
+    }
+
+    public Page<SpecDevice> getAllDevicesListByFile(Long id, Integer page, int resultPerPageLimit, String sortBy, String order) {
+        return repository.findAllByFileId(id, PageRequest.of(page, resultPerPageLimit, PageAndSortUtil.getSorting(sortBy, order)));
+
+    }
+
+    @Override
+    public void updateDevice(CsvBean device) {
+        repository.save((SpecDevice) device);
+    }
+}
